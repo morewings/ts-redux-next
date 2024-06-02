95fef37d539c5ab59a8e3bdea997ffea8eedfecc
Dima Vyshniakov <morewings@gmail.com>
2024-06-02 14:25:56 +0200

    Upgrade ESlint to 9

--- src/components/Random/Random.spec.tsx
+++ src/components/Random/Random.spec.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import {Provider} from 'react-redux';
 import type {Store, Action} from 'redux';
 import {
--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,4 +1,3 @@
 // jest-dom adds custom jest matchers for asserting on DOM nodes.
 // learn more: https://github.com/testing-library/jest-dom
-// eslint-disable-next-line import/no-extraneous-dependencies
 import '@testing-library/jest-dom';
